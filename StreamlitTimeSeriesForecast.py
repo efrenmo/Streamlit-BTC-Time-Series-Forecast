37689f9ac414fb508920e390870a055255ea3814
Efren A. Mora <53714269+efrenmo@users.noreply.github.com>
2022-10-22 01:28:58 -0400

    clearing typost

--- StreamlitTimeSeriesForecast.py
+++ StreamlitTimeSeriesForecast.py
@@ -46,7 +46,7 @@ from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error
 st.set_page_config(
     page_title="Time Series Forecast Project",
     layout='centered', #wide
-    initial_sidebar_state= "collapsed", #"expanded", #collapsed
+    initial_sidebar_state= "expanded", #"expanded", #collapsed
     menu_items={
         'Report a bug': "https://github.com/efrenmo"
     }
@@ -112,9 +112,8 @@ with st.sidebar:
     #    key=None,
     #)
 
-    #st.markdown("< style='text-align: center;'>Table of contents</h1>", unsafe_allow_html=True) #; color: black
+    #st.markdown("<h1 style='text-align: center;'>Table of contents</h1>", unsafe_allow_html=True) #; color: black
     st.title("Table of contents")  
-
 
 
     toc.placeholder()
@@ -130,11 +129,18 @@ with st.container():
     tab1, tab2, tab3 = st.tabs(["Introduction", "Data Source", "Methodology"])
     
     with tab1:
-        st.write('*Click on the desired tab')
+        #st.write('*Click on the desired tab')
+        st.markdown("""
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hand-index-thumb" viewBox="0 0 16 16">
+        <path d="M6.75 1a.75.75 0 0 1 .75.75V8a.5.5 0 0 0 1 0V5.467l.086-.004c.317-.012.637-.008.816.027.134.027.294.096.448.182.077.042.15.147.15.314V8a.5.5 0 0 0 1 0V6.435l.106-.01c.316-.024.584-.01.708.04.118.046.3.207.486.43.081.096.15.19.2.259V8.5a.5.5 0 1 0 1 0v-1h.342a1 1 0 0 1 .995 1.1l-.271 2.715a2.5 2.5 0 0 1-.317.991l-1.395 2.442a.5.5 0 0 1-.434.252H6.118a.5.5 0 0 1-.447-.276l-1.232-2.465-2.512-4.185a.517.517 0 0 1 .809-.631l2.41 2.41A.5.5 0 0 0 6 9.5V1.75A.75.75 0 0 1 6.75 1zM8.5 4.466V1.75a1.75 1.75 0 1 0-3.5 0v6.543L3.443 6.736A1.517 1.517 0 0 0 1.07 8.588l2.491 4.153 1.215 2.43A1.5 1.5 0 0 0 6.118 16h6.302a1.5 1.5 0 0 0 1.302-.756l1.395-2.441a3.5 3.5 0 0 0 .444-1.389l.271-2.715a2 2 0 0 0-1.99-2.199h-.581a5.114 5.114 0 0 0-.195-.248c-.191-.229-.51-.568-.88-.716-.364-.146-.846-.132-1.158-.108l-.132.012a1.26 1.26 0 0 0-.56-.642 2.632 2.632 0 0 0-.738-.288c-.31-.062-.739-.058-1.05-.046l-.048.002zm2.094 2.025z"/>
+        </svg> Click on the desired tab 
+        """, unsafe_allow_html=True)
+        
+        
         toc.header("Introduction")
         
         st.write("""
-        In this notebook I'll use [**Prophet**](https://facebook.github.io/prophet/) time series forecasting 
+        In this project I'll use [**Prophet's**](https://facebook.github.io/prophet/) time series forecasting 
         algorithm to attempt to forecast Bitcoin's price for the next thrirty days. 
 
         [**Prophet**](https://facebook.github.io/prophet/)  is an open source forecasting package that was
@@ -164,7 +170,7 @@ with st.container():
 
     with tab2:
         st.header("Data Source")
-        #st.header('Data Source')
+        
         st.write("""
         For this project I'll be using data from `May 11th, 2020 to September 16th, 2022`. The profile of this asset has change
         dramatically in recent years, that is why I'm choosing to use relatively newer data; and is around this time when a pivot
@@ -280,6 +286,7 @@ st.dataframe(BTC_Data.style.format({'Close':'{:.2f}','Volume':'{:.2f}','Low':'{:
 
 st.subheader('Data Statistics')
 st.write(BTC_Data.describe()) 
+
   
 # ---------- SECTION 3: Visualizing the Data Set ---------- #    
 
@@ -419,8 +426,12 @@ with tab2:
         chart.show()
     
 
-with tab1:
-    st.write('*Click on the "Code" tab to see the code for this chart')
+with tab1:    
+    st.markdown("""
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hand-index-thumb" viewBox="0 0 16 16">
+        <path d="M6.75 1a.75.75 0 0 1 .75.75V8a.5.5 0 0 0 1 0V5.467l.086-.004c.317-.012.637-.008.816.027.134.027.294.096.448.182.077.042.15.147.15.314V8a.5.5 0 0 0 1 0V6.435l.106-.01c.316-.024.584-.01.708.04.118.046.3.207.486.43.081.096.15.19.2.259V8.5a.5.5 0 1 0 1 0v-1h.342a1 1 0 0 1 .995 1.1l-.271 2.715a2.5 2.5 0 0 1-.317.991l-1.395 2.442a.5.5 0 0 1-.434.252H6.118a.5.5 0 0 1-.447-.276l-1.232-2.465-2.512-4.185a.517.517 0 0 1 .809-.631l2.41 2.41A.5.5 0 0 0 6 9.5V1.75A.75.75 0 0 1 6.75 1zM8.5 4.466V1.75a1.75 1.75 0 1 0-3.5 0v6.543L3.443 6.736A1.517 1.517 0 0 0 1.07 8.588l2.491 4.153 1.215 2.43A1.5 1.5 0 0 0 6.118 16h6.302a1.5 1.5 0 0 0 1.302-.756l1.395-2.441a3.5 3.5 0 0 0 .444-1.389l.271-2.715a2 2 0 0 0-1.99-2.199h-.581a5.114 5.114 0 0 0-.195-.248c-.191-.229-.51-.568-.88-.716-.364-.146-.846-.132-1.158-.108l-.132.012a1.26 1.26 0 0 0-.56-.642 2.632 2.632 0 0 0-.738-.288c-.31-.062-.739-.058-1.05-.046l-.048.002zm2.094 2.025z"/>
+        </svg> Click on the "Code" tab to see the code for this chart
+        """, unsafe_allow_html=True)
     st.plotly_chart(chart)
 
 # ---------- SECTION 4: Data  Preparation  ---------- #
@@ -806,8 +817,13 @@ with tab2:
                             )             
                 )
 
-with tab1:   
-    st.write('*Click on the "Code" tab to see the code for this chart')
+with tab1:
+    st.markdown("""
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hand-index-thumb" viewBox="0 0 16 16">
+        <path d="M6.75 1a.75.75 0 0 1 .75.75V8a.5.5 0 0 0 1 0V5.467l.086-.004c.317-.012.637-.008.816.027.134.027.294.096.448.182.077.042.15.147.15.314V8a.5.5 0 0 0 1 0V6.435l.106-.01c.316-.024.584-.01.708.04.118.046.3.207.486.43.081.096.15.19.2.259V8.5a.5.5 0 1 0 1 0v-1h.342a1 1 0 0 1 .995 1.1l-.271 2.715a2.5 2.5 0 0 1-.317.991l-1.395 2.442a.5.5 0 0 1-.434.252H6.118a.5.5 0 0 1-.447-.276l-1.232-2.465-2.512-4.185a.517.517 0 0 1 .809-.631l2.41 2.41A.5.5 0 0 0 6 9.5V1.75A.75.75 0 0 1 6.75 1zM8.5 4.466V1.75a1.75 1.75 0 1 0-3.5 0v6.543L3.443 6.736A1.517 1.517 0 0 0 1.07 8.588l2.491 4.153 1.215 2.43A1.5 1.5 0 0 0 6.118 16h6.302a1.5 1.5 0 0 0 1.302-.756l1.395-2.441a3.5 3.5 0 0 0 .444-1.389l.271-2.715a2 2 0 0 0-1.99-2.199h-.581a5.114 5.114 0 0 0-.195-.248c-.191-.229-.51-.568-.88-.716-.364-.146-.846-.132-1.158-.108l-.132.012a1.26 1.26 0 0 0-.56-.642 2.632 2.632 0 0 0-.738-.288c-.31-.062-.739-.058-1.05-.046l-.048.002zm2.094 2.025z"/>
+        </svg> Click on the desired
+        """, unsafe_allow_html=True)   
+    
     toc.subheader('Performance Metric Visualization') 
     st.plotly_chart(figure2)
 
@@ -1200,8 +1216,13 @@ with tab2:
                             )             
                 )
     with tab1:   
-        st.write('*Click on the "Code" tab to see the code for this chart')
-        #toc.subheader('P2 Performance Metric Visualization') 
+        st.markdown("""
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hand-index-thumb" viewBox="0 0 16 16">
+        <path d="M6.75 1a.75.75 0 0 1 .75.75V8a.5.5 0 0 0 1 0V5.467l.086-.004c.317-.012.637-.008.816.027.134.027.294.096.448.182.077.042.15.147.15.314V8a.5.5 0 0 0 1 0V6.435l.106-.01c.316-.024.584-.01.708.04.118.046.3.207.486.43.081.096.15.19.2.259V8.5a.5.5 0 1 0 1 0v-1h.342a1 1 0 0 1 .995 1.1l-.271 2.715a2.5 2.5 0 0 1-.317.991l-1.395 2.442a.5.5 0 0 1-.434.252H6.118a.5.5 0 0 1-.447-.276l-1.232-2.465-2.512-4.185a.517.517 0 0 1 .809-.631l2.41 2.41A.5.5 0 0 0 6 9.5V1.75A.75.75 0 0 1 6.75 1zM8.5 4.466V1.75a1.75 1.75 0 1 0-3.5 0v6.543L3.443 6.736A1.517 1.517 0 0 0 1.07 8.588l2.491 4.153 1.215 2.43A1.5 1.5 0 0 0 6.118 16h6.302a1.5 1.5 0 0 0 1.302-.756l1.395-2.441a3.5 3.5 0 0 0 .444-1.389l.271-2.715a2 2 0 0 0-1.99-2.199h-.581a5.114 5.114 0 0 0-.195-.248c-.191-.229-.51-.568-.88-.716-.364-.146-.846-.132-1.158-.108l-.132.012a1.26 1.26 0 0 0-.56-.642 2.632 2.632 0 0 0-.738-.288c-.31-.062-.739-.058-1.05-.046l-.048.002zm2.094 2.025z"/>
+        </svg> Click on the "Code" tab to see the code for this chart
+        """, unsafe_allow_html=True)
+        
+        #st.subheader('P2 Performance Metric Visualization') 
         st.plotly_chart(figure4)
 
 
